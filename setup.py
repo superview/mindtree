aeb4847d534b1fc1d127503ea51d9dc982d0e0df
ron.longo <ron.longo@cox.net>
2008-07-31 23:27:37 +0000

    Updated for a googlecode installer.

--- setup.py
+++ setup.py
@@ -1,23 +1,39 @@
-# setup.py
-import glob
-import os
-import sys
 from distutils.core import setup
-import py2exe
+import enchant
 
-def files( folder ):
-   for path in glob.glob( folder + '\\*' ):
-      if os.path.isfile( path ):
-         yield path
+setup( name='MindTree',
+       version='0.1.0',
+       description='Notes organizer/Outliner/PIM.',
+       long_description='MindTree is an outliner application designed for taking and organizing notes and publishing these notes to the web.',
+       url='http://code.google.com/p/mindtree',
+       author='Ron Longo',
+       author_email='ron.longo@cox.net',
+       license='Apache License 2.0',
+       data_files=[ ( 'PyEnchant', enchant.utils.win32_data_files() ) ],
+       classifiers=[
+                      'Development Status :: 3 - Alpha',
+                      'Environment :: Tkinter',
+                      'Intended Audience :: Developers',
+                      'Intended Audience :: Education',
+                      'Intended Audience :: End Users/Desktop',
+                      'Intended Audience :: Financial and Insurance Industry',
+                      'Intended Audience :: Healthcare Industry',
+                      'Intended Audience :: Information Technology',
+                      'Intended Audience :: Legal Indstry',
+                      'Intended Audience :: Manufacturing',
+                      'Intended Audience :: Other Audience',
+                      'Intended Audience :: Religion',
+                      'Intended Audience :: Science/Research',
+                      'Intended Audience :: System Administrators',
+                      'Intended Audience :: Telecommunications Industry',
+                      'License :: OSI Approved :: Apache Software License',
+                      'Natural Language :: English',
+                      'Programming Language :: Python',
+                      'Topic :: Documentation',
+                      'Topic :: Education',
+                      'Topic :: Internet :: WWW/HTTP',
+                      'Topic :: Office/Business',
+                      'Topic :: Scientific/Engineering :: Information Analysis'
+                   ]
+     )
 
-data_files=[
-           ( '.', glob.glob(sys.prefix + '\\DLLs\\tix8184.dll' )),
-           ( 'tcl\\tix8.1', files(sys.prefix+'\\tcl\\tix8.1')),
-           ( 'tcl\\tix8.1\\bitmaps', files( sys.prefix + '\\tcl\tix8.1\bitmaps')),
-           ( 'tcl\\tix8.1\pref', files(sys.prefix+'\\tcl\\tix8.1\\pref')),
-           ]
-
-for entry in data_files:
-   print entry
-
-setup(windows=["MindTree.py"], packages=[ "TkTools" ])
