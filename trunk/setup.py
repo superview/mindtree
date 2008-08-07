17f3fd68835847e44aa3bacce637865614db6a42
ron.longo@cox.net <ron.longo@cox.net@8e12456e-e352-0410-961a-1f49d853a389>
2008-08-07 17:40:42 +0000

    Added a README.txt to assist the user.

--- trunk/setup.py
+++ trunk/setup.py
@@ -8,7 +8,15 @@ try:
 except:
    pass
 
-#import enchant
+name='MindTree'
+version='1.0.0-a002'
+pythonVersion='%d.%d' % (sys.version_info[:2])
+descr='Notes organizer/Outliner/PIM for Python %s.' % pythonVersion
+long_descr='MindTree is an outliner application designed for taking and organizing notes and publishing these notes to the web.  Requires Python %s.' % pythonVersion
+author='Ron Longo'
+author_email='ron.longo@cox.net'
+proj_url='http://code.google.com/p/mindtree'
+dist_url=proj_url + '/downloads/list'
 
 def files(folder):
    for path in glob.glob(folder+'/*'):
@@ -20,44 +28,44 @@ tixDataFiles = [
                ('tcl/tix8.4', files(sys.prefix+'/tcl/tix8.4')),
                ('tcl/tix8.4/bitmaps', files(sys.prefix+'/tcl/tix8.4/bitmaps')),
                ('tcl/tix8.4/pref', files(sys.prefix+'/tcl/tix8.4/pref')),
+	       #('PyEnchant', enchant.utils.win32_data_files() )
                ]
 
-setup( name='MindTree',
-       version='1.0.0-a002',
-       description='Notes organizer/Outliner/PIM for Python 2.5.',
-       long_description='MindTree is an outliner application designed for taking and organizing notes and publishing these notes to the web.  Requires Python 2.5.',
-       author='Ron Longo',
-       author_email='ron.longo@cox.net',
-       maintainer='Ron Longo',
-       maintainer_email='ron.longo@cox.net',
-       url='http://code.google.com/p/mindtree',
-       download_url='http://code.google.com/p/mindtree/downloads/list',
+setup( name=name,
+       version=version,
+       description=descr,
+       long_description=long_descr,
+       author=author,
+       author_email=author_email,
+       maintainer=author,
+       maintainer_email=author_email,
+       url=proj_url,
+       download_url=dist_url,
        license='Apache License 2.0',
        windows=['MindTree.py'],
        data_files=tixDataFiles,
-#       data_files=[ ( 'PyEnchant', enchant.utils.win32_data_files() ) ],
        classifiers=[
-                      'Development Status :: 3 - Alpha',
-                      'Intended Audience :: Developers',
-                      'Intended Audience :: Education',
-                      'Intended Audience :: End Users/Desktop',
-                      'Intended Audience :: Financial and Insurance Industry',
-                      'Intended Audience :: Healthcare Industry',
-                      'Intended Audience :: Information Technology',
-                      'Intended Audience :: Manufacturing',
-                      'Intended Audience :: Other Audience',
-                      'Intended Audience :: Religion',
-                      'Intended Audience :: Science/Research',
-                      'Intended Audience :: System Administrators',
-                      'Intended Audience :: Telecommunications Industry',
-                      'License :: OSI Approved :: Apache Software License',
-                      'Natural Language :: English',
-                      'Programming Language :: Python',
-                      'Topic :: Documentation',
-                      'Topic :: Education',
-                      'Topic :: Internet :: WWW/HTTP',
-                      'Topic :: Office/Business',
-                      'Topic :: Scientific/Engineering :: Information Analysis'
+                   'Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Education',
+                   'Intended Audience :: End Users/Desktop',
+                   'Intended Audience :: Financial and Insurance Industry',
+                   'Intended Audience :: Healthcare Industry',
+                   'Intended Audience :: Information Technology',
+                   'Intended Audience :: Manufacturing',
+                   'Intended Audience :: Other Audience',
+                   'Intended Audience :: Religion',
+                   'Intended Audience :: Science/Research',
+                   'Intended Audience :: System Administrators',
+                   'Intended Audience :: Telecommunications Industry',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Natural Language :: English',
+                   'Programming Language :: Python',
+                   'Topic :: Documentation',
+                   'Topic :: Education',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Office/Business',
+                   'Topic :: Scientific/Engineering :: Information Analysis'
                    ]
      )
 
