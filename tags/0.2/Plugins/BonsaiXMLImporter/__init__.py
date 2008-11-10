4e0be7cfc241fff95fd7eda021359a3de7855de9
ron.longo@cox.net <ron.longo@cox.net@8e12456e-e352-0410-961a-1f49d853a389>
2008-11-10 15:17:08 +0000

    This is the final version of MindTree Tk for Python 2.5.

---
+++ tags/0.2/Plugins/BonsaiXMLImporter/__init__.py
@@ -0,0 +1,20 @@
+from __future__ import print_function, unicode_literals
+from tkApplicationFramework import ImporterPlugin
+
+class BonsaiXMLArchiver( ImporterPlugin ):
+   NAME                   = 'Bonsai XML...'
+   DEFAULT_SETTINGS       = { }
+   
+   FILE_TYPES             = [ ( 'Bonzai XML File', '*.xml' ), ( 'All Files', '*.*' ) ]
+   FILE_EXTENSION         = '.xml',
+
+   def __init__( self, aView ):
+      ImporterPlugin.__init__( self, aView, BonsaiXMLArchiver.FILE_TYPES, BonsaiXMLArchiver.FILE_EXTENSION, self.CONFIG.get( 'Workspace', 'directory' ) )
+   
+   def _readFile( self, filename ):
+      import BonsaiXMLImporter
+      return BonsaiXMLImporter.Parser.parseXML(filename)
+
+
+pluginClass = BonsaiXMLArchiver
+
